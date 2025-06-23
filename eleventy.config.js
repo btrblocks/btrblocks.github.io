4bc51fd0c8fb6917812969abd171314c9a03a340
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-06-23 14:38:16 +0200

    More preparations for blog.

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,6 +5,7 @@ import pluginNavigation from "@11ty/eleventy-navigation";
 import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 import svgContents from "eleventy-plugin-svg-contents"
 import faviconPlugin from "eleventy-favicon"
+import footnotePlugin from 'markdown-it-footnote';
 
 import pluginFilters from "./_config/filters.js";
 
@@ -100,6 +101,8 @@ export default async function(eleventyConfig) {
     eleventyConfig.addPlugin(svgContents);
     // generate favicons automatically from our logo svg
     eleventyConfig.addPlugin(faviconPlugin);
+    // footnotes in markdown
+	eleventyConfig.amendLibrary("md", (mdLib) => mdLib.use(footnotePlugin));
 
 	// Filters
 	eleventyConfig.addPlugin(pluginFilters);
